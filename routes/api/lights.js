1021f49eaca0e90658857f2d01f5caebf0cf86a2
christopherfelt <chris.h.felt@gmail.com>
2020-12-14 07:43:19 -0700

    added put request

--- routes/api/lights.js
+++ routes/api/lights.js
@@ -27,4 +27,17 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.put("/:id", async(req, res)=>  {
+    try {
+      const light = await Light.findOneAndUpdate({_id:id}, req.body, {new:true});
+
+      if(!light){
+        throw Error("bad data/ put request");
+      }
+      res.status(200).json(light)
+    } catch (error) {
+      res.status(400).json({msg: error.message});
+    }
+})
+
 export default router;
